5ebe198c591bd107afdcd80736c63161e5fdf790
schoebel <schoebel@bell.site>
2012-06-06 18:40:43 +0200

    scripts: make conversion more robust

--- scripts/conv_blktrace_to_load.sh
+++ scripts/conv_blktrace_to_load.sh
@@ -31,6 +31,7 @@
 # TST 2010-01-26
 
 filename="$1"
+action_char="${action_char:-}" # allow override from extern
 
 if ! [ -f "$filename.blktrace.0" ]; then
     echo "Input file $filename.blktrace.0 does not exist"
@@ -44,16 +45,46 @@ source "$script_dir/modules/lib.sh" || exit $?
 check_list="grep sed cut gzip blkparse"
 check_installed "$check_list"
 
+if [ -z "$action_char" ]; then
+    echo "Computing \$action_char ..."
+    tmp="${TMPDIR:-/tmp}/blkparse.$$"
+    mkdir -p $tmp || exit $?
+    blkparse -v -i "$filename" -f '%a:\n' |\
+	grep "^[QGID]:$" >\
+	$tmp/actions || exit $?
+    char_list="$(sort -u < $tmp/actions)" || exit $?
+    echo "Statistics:"
+    for i in $char_list; do
+	echo "$i$(grep "$i" < $tmp/actions | wc -l)"
+    done | sort -t: -k2 -n -r | tee $tmp/list
+    action_char="$(head -n1 < $tmp/list | cut -d: -f1)"
+    rm -rf $tmp
+    if [ -z "$action_char" ]; then
+	echo "Sorry, cannot determine the right action character for blkparse."
+	echo "In case absolutely nothing else is available for determining"
+	echo "the stating points, try the completion points by setting"
+	echo "action_char='C' as a last resort. But check the output"
+	echo "for plausibility then..."
+	exit -1
+    fi
+fi
+
+echo "Using action_char='$action_char'"
+
+echo "Starting main conversion to '$filename.load.gz'..."
+
 {
     echo_copyright "$filename.blktrace.*"
     echo "start ; sector; length ; op ;  replay_delay=0 ; replay_duration=0"
     
-    blkparse -v -i "$filename" -f '%2a ; %6T.%9t ; %12S ; %4n ; %3d ; 0.0 ; 0.0\n' |\
-	sed 's/^ Q/ D/' |\
+    blkparse -v -i "$filename" -f '%a; %6T.%9t ; %12S ; %4n ; %3d ; 0.0 ; 0.0\n' |\
+	grep "^$action_char;" |\
 	sed 's/ WB/  W/' |\
 	sed 's/ WS /  W /' |\
 	sed 's/  \([RW]\)/\1/' |\
-	grep "^ D" |\
+	grep '; [RW] ;' |\
 	cut -d';' -f2-
 } | gzip -9 > "$filename.load.gz"
 
+ls -l "$filename.load.gz"
+echo "Done. Please consider renaming the output file to something better."
