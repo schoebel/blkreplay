3037fd12ba8b07c2a1534bb80955589f08f62312
schoebel <schoebel@bell.site>
2012-06-10 18:41:56 +0200

    scripts: speedup creation of derived loads

--- scripts/create_derived_load.sh
+++ scripts/create_derived_load.sh
@@ -23,7 +23,8 @@
 #
 # The original loads "$@" are split into snippets having length $window each.
 # The snippets are sorted according to IOPS and distributed to
-# 0..($max-1) output files in a round-robin fashion.
+# 0..($max-1) output files in a round-robin fashion, where
+# max is iterating through the list $split_list.
 #
 # The rationale is explained at
 # http://www.blkreplay.org/loads/natural/1and1/natural-derived/00README
@@ -38,76 +39,96 @@ source "$script_dir/modules/lib.sh" || exit $?
 check_list="gzip gunzip grep sed gawk cut"
 check_installed "$check_list"
 
-window=${window:-600}
-max=${max:-32}
+window=${window:-300}
+split_list=${split_list:-001 002 004 008 016 032 064 128}
+
 
 function paste_together
 {
-    gawk -F";" 'BEGIN{ old = 0.0; offset = 0.0; } { if ($1 < old) offset += old; printf("%17.9f ;%s;%s;%s;%s;%s\n", $1 + offset, $2, $3, $4, $5, $6); old = $1; }'
+    gawk -F";" 'BEGIN{ old = 0.0; offset = 0.0; } { if ($1 < old) { offset += old; } printf("%17.9f ;%s;%s;%s;%s;%s\n", $1 + offset, $2, $3, $4, $5, $6); old = $1; }'
 }
 
 function from_start
 {
-    gawk -F";" 'BEGIN{ offset = -1; } { if (offset < 0) offset = $1; printf("%17.9f ;%s;%s;%s;%s;%s\n", $1 - offset, $2, $3, $4, $5, $6); }'
+    gawk -F";" 'BEGIN{ offset = -1; } { if (offset < 0) { offset = $1; } printf("%17.9f ;%s;%s;%s;%s;%s\n", $1 - offset, $2, $3, $4, $5, $6); }'
 }
 
 tmp="${TMPDIR:-/tmp}/snippets.$$"
 mkdir -p $tmp/s || exit $?
 
-echo "List of output files:"
-for i in $(eval "echo {0..$(($max-1))}"); do
-    base="$(basename $1 | sed 's/\.\(load\|gz\|[0-9]\+\)//g')"
-    out[i]="$(printf "$base.derived.%03d.load.gz" $i)"
-    list[i]=""
-    echo "  ${out[i]}"
-done
-
-echo "Creating snippets. This may take a long time...."
+echo "Creating snippets. This may take a long time...." 1>&2
 limit=$window
 snippet=0
-count=0
+total=0
 cat "$@" |\
     gunzip -f |\
     grep ";" |\
     grep -v "[a-z]" |\
-    #head -n 200000 |\
     paste_together |\
-    while IFS="." read time rest; do
-    if (( time >= limit )); then
+{
+    count=0
+    while true; do
+        while IFS="." read time rest; do
+	    if (( time >= limit )); then
+		break;
+	    fi
+	    echo "$time.$rest"
+	    (( count++ ))
+	done >> $tmp/xxx
+	if [ -z "$time" ]; then
+	    break
+	fi
 	(( iops = count / window ))
-	echo "  snippet $snippet at $limit has $count requests ($iops IOPS)"
+	echo "  snippet $snippet at $limit has $count requests ($iops IOPS)" 1>&2
+	(( total += count ))
 	from_start < $tmp/xxx > $tmp/s/$count.$snippet
 	rm -f $tmp/xxx
-	count=0
 	(( snippet++ ))
 	(( limit += window ))
-    fi
-    echo "$time.$rest" >> $tmp/xxx
-    (( count++ ))
+	echo "$time.$rest" > $tmp/xxx
+	count=1
+    done
+    echo "------------------------------------------------" 1>&2
+    echo "snippets      : $snippet" 1>&2
+    echo "reworked lines: $total" 1>&2
+    echo "rest     lines: $count" 1>&2
+    echo "sum      lines: $(( total + count ))" 1>&2
+    echo "average  IOPS : $(( total / snippet / window ))" 1>&2
+    echo "------------------------------------------------" 1>&2
+}
+
+base="$(basename "$1" | sed 's/\.\(load\|gz\|[0-9]\+\)//g')"
+
+for max in $split_list; do
+    max_num=$(echo $max | sed 's/^0*//')
+    dir="${base}.derived.split.$max"
+    mkdir -p "$dir" || exit $?
+    echo "------------------------------------------------" 1>&2
+    echo "Splitting into $max:" 1>&2
+    for i in $(eval "echo {0..$(($max_num - 1))}"); do
+	out[i]="$(printf "$base.derived.%03d.of.$max_num.load.gz" $i)"
+	list[i]=""
+    done
+
+    j=0
+    for i in $( (cd $tmp/s && ls) | sort -n -r ); do
+	#echo "$i => $j" 1>&2
+	list[j]="${list[j]} $i"
+	(( j = (j + 1) % max_num ))
+    done
+
+    for i in $(eval "echo {0..$(($max_num - 1))}"); do
+	[ -z "${list[i]}" ] && continue
+	echo "${out[i]} ${list[i]}" 1>&2
+	(
+	    echo_copyright "$(basename "$1")..."
+	    echo "start ; sector; length ; op ;  replay_delay ; replay_duration"
+	    cd $tmp/s
+	    cat ${list[i]} | paste_together
+	) | gzip -8 > "$dir/${out[i]}" &
+    done
+    echo "Waiting for termination of subprocesses...." 1>&2
+    wait
 done
-
-echo "------------------------------------------------"
-
-j=0
-for i in $( (cd $tmp/s && ls) | sort -n -r ); do
-    echo "$i => $j"
-    list[j]="${list[j]} $i"
-    (( j = (j+1) % max ))
-done
-
-echo "Assignment of snippets to output files:"
-for i in $(eval "echo {0..$(($max-1))}"); do
-    [ -z "${list[i]}" ] && continue
-    echo "${out[i]} ${list[i]}"
-    (
-	echo_copyright "$(basename "$1")..."
-	echo "start ; sector; length ; op ;  replay_delay ; replay_duration"
-	cd $tmp/s
-	cat ${list[i]} | paste_together
-    ) | gzip -8 > ${out[i]} &
-done
-
-echo "Waiting for termination of subprocesses...."
-wait
 
 rm -rf $tmp
